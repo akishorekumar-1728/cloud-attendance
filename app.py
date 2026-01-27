2f955c552e13f7039b338998e3fcaf1c6845dc89
akishorekumar-1728 <akishorekumar1728@gmail.com>
2026-01-27 18:25:09 +0530

    Initial commit: Flask OTP Attendance + Docker

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, request
+import sqlite3, random, time
+
+app = Flask(__name__)
+
+# ---------- DATABASE ----------
+def get_db():
+    return sqlite3.connect("database.db")
+
+def create_tables():
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS otp (
+        code TEXT,
+        created_time INTEGER
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS attendance (
+        student_email TEXT,
+        timestamp INTEGER
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+create_tables()
+
+# ---------- LOGIN ----------
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        role = request.form['role']
+        email = request.form['email']
+
+        if role == "admin":
+            return render_template('admin.html')
+        elif role == "teacher":
+            return render_template('teacher.html')
+        elif role == "student":
+            return render_template('student.html', email=email)
+
+    return render_template('login.html')
+
+# ---------- OTP ----------
+@app.route('/generate_otp', methods=['POST'])
+def generate_otp():
+    otp = str(random.randint(100000, 999999))
+    ts = int(time.time())
+
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM otp")
+    cur.execute("INSERT INTO otp VALUES (?, ?)", (otp, ts))
+    conn.commit()
+    conn.close()
+
+    return f"OTP Generated: {otp} (valid 30s)"
+
+# ---------- STUDENT SUBMIT ----------
+@app.route('/submit_otp', methods=['POST'])
+def submit_otp():
+    otp = request.form['otp']
+    email = request.form['email']
+
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT code, created_time FROM otp")
+    data = cur.fetchone()
+
+    if data:
+        db_otp, created = data
+        if otp == db_otp and int(time.time()) - created <= 30:
+            cur.execute("INSERT INTO attendance VALUES (?, ?)", (email, int(time.time())))
+            conn.commit()
+            conn.close()
+            return "Attendance Marked ✅"
+
+    conn.close()
+    return "Invalid or Expired OTP ❌"
+
+# ---------- VIEW ATTENDANCE ----------
+@app.route('/view_attendance')
+def view_attendance():
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM attendance")
+    data = cur.fetchall()
+    conn.close()
+    return render_template("admin.html", records=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
